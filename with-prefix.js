d36dc69c510951118bb10ffe280bee2534b7ee00
Gertt <Gertt@users.noreply.github.com>
2017-12-13 15:30:49 +0100

    add load-env factory with automatic environment prefixes

---
+++ with-prefix.js
@@ -0,0 +1,46 @@
+const Fs = require('fs');
+const Path = require('path');
+const callerId = require('caller-id');
+const json = require('@ambassify/json-parse-safe');
+const load_env = require('./index');
+
+function findPackageName(from) {
+    let contents;
+
+    do {
+        const pkg = Path.join(from, 'package.json');
+
+        if (Fs.existsSync(pkg))
+            contents = json(Fs.readFileSync(pkg, { encoding: 'utf-8' }));
+
+        const parent = Path.dirname(from);
+        from = from === parent ? null : parent;
+
+    } while (from && !contents)
+
+    return (contents || {}).name;
+}
+
+module.exports = function createLoadEnvWithPrefix(prefix) {
+    if (!prefix) {
+        const caller = callerId.getData().filePath;
+        let packageName = findPackageName(caller);
+        
+        if (packageName) {
+            // remove scope if available
+            packageName = packageName.split('/').pop();
+            prefix = packageName.toUpperCase().replace(/[^a-z0-9]+/gi, '_') + '_';
+        }
+    }
+
+    return (key, ...args) => {
+        try {
+            return load_env(`${prefix}${k}`, ...args);
+        } catch (e) {
+            if (typeof process.env[`${prefix}${key}`] === 'undefined')
+                return load_env(key, ...args);
+            else
+                throw e;
+        }
+    };
+}
